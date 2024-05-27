6959e2e13a01eec44a09cb8a846b965c42bfbd12
MTA111 <133819586+MTA111@users.noreply.github.com>
2024-05-27 17:26:13 +0300

    Update config_flow.py

--- custom_components/hass_nuki_bt/config_flow.py
+++ custom_components/hass_nuki_bt/config_flow.py
@@ -129,9 +129,9 @@ class NukiFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             return await self.async_step_step1(user_input)
 
         app_id = random.getrandbits(32)
-        keypair = PrivateKey.generate()
-        public_key = bytes(keypair.public_key)
-        private_key = bytes(keypair)
+        key pair = PrivateKey.generate()
+        public_key = bytes(key pair.public_key)
+        private_key = bytes(key pair)
 
         if self._data[CONF_CLIENT_TYPE] == "App":
             client_type = NukiConst.NukiClientType.APP
